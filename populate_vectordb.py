2aa5f973cf3d307ac8e6126291ee684f04b6cf6f
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-06-16 22:36:07 -0400

    fix: debuug for the populate function

--- populate_vectordb.py
+++ populate_vectordb.py
@@ -2,13 +2,16 @@ from qdrant_client import QdrantClient, models
 from qdrant_client.models import PointStruct
 from dotenv import load_dotenv
 from transformers import AutoTokenizer, TFAutoModel
-
-import os, fitz, tqdm
+from tqdm import tqdm
+from fitz import open as fitz_open
+import os
 import tensorflow as tf
 
 load_dotenv()
 
 FOLDER_DOCS = "data\docs_only"
+COLLECTIONS = ["document_only", "image_only"]
+
 
 # Get the Url and API for qdrant, from the .env file:
 qdrant_url = os.getenv("QDRANT_URL")
@@ -20,34 +23,27 @@ qdrant_client = QdrantClient(
 )
 
 # Tokenizer and Embedding:
-tokenizer = AutoTokenizer("sentence-transformers/all-MiniLM-L6-v2")
-model = TFAutoModel("sentence-transformers/all-MiniLM-L6-v2")
-
-
-
-collections = ["document_only", "image_only"]
-
-
+tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+model = TFAutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
 
 def embedding(text):
-    tokens = tokenizer(text, return_tensor = "pt", truncation = True, padding = True)
+    tokens = tokenizer(text, return_tensors = "tf", truncation = True, padding = True)
     output = model(**tokens)
     embedding = tf.reduce_mean(output.last_hidden_state, axis = 1)
     return embedding.numpy().tolist()
 
 def extract_text_from_pdf(pdf_path):
     text = ""
-    with fitz.open(pdf_path) as pdf:
+    with fitz_open(pdf_path) as pdf:
         for page in pdf:
             text += page.get_text()
     return text
 
 
 try:
-
     # Create the Collections:
-    for collection in tqdm(collections) :
+    for collection in tqdm(COLLECTIONS) :
         if not qdrant_client.collection_exists(collection_name = "document_only"):
             print(f"\033[93m Create collection {collection} \033[0m")
             qdrant_client.create_collection(
@@ -58,9 +54,12 @@ try:
         else:
             print(f"\033[92m Collection {collection} already exists! \033[0m")
 
-    print(f"Collections n Qdrant are {qdrant_client.get_collections()}")
+    print(f"COLLECTIONS n Qdrant are {qdrant_client.get_collections()}")
 
     # Embedding:
+
+    points_list = []
+
     for idx, pdf_file in tqdm(enumerate(os.listdir(FOLDER_DOCS), start=1 )):
         if pdf_file.endswith(".pdf"):
             pdf_path = os.path.join(FOLDER_DOCS, pdf_file)
@@ -81,11 +80,11 @@ try:
                     "content": text[:500]
                 }
             )
-
-            qdrant_client.upsert(
-                collection_name= "document_only",
-                points= point
-            )
+            points_list.append(point)
+    qdrant_client.upsert(
+        collection_name= "document_only",
+        points= points_list
+    )
     print("\033[92m Embedding Done! \033[0m")
 except Exception as e:
     print(f"\033[91m {e} \033[0m")
