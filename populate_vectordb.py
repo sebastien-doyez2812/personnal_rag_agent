a6a2aead32d7291050dfa597b7969d6ae2232a6a
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-06-16 22:12:54 -0400

    Flow of the embedding

--- populate_vectordb.py
+++ populate_vectordb.py
@@ -1,8 +1,9 @@
 from qdrant_client import QdrantClient, models
+from qdrant_client.models import PointStruct
 from dotenv import load_dotenv
 from transformers import AutoTokenizer, TFAutoModel
 
-import os
+import os, fitz, tqdm
 import tensorflow as tf
 
 load_dotenv()
@@ -26,18 +27,7 @@ model = TFAutoModel("sentence-transformers/all-MiniLM-L6-v2")
 
 collections = ["document_only", "image_only"]
 
-for collection in collections:
-    if not qdrant_client.collection_exists(collection_name = "document_only"):
-        print(f"\033[93m Create collection {collection} \033[0m")
-        qdrant_client.create_collection(
-            collection_name="document_only",
-            vectors_config = models.VectorParams(size=1024, distance = models.Distance.COSINE)
-        )
-        print(f"\033[92m Collection {collection} created! \033[0m")
-    else:
-        print(f"\033[92m Collection {collection} already exists! \033[0m")
 
-print(qdrant_client.get_collections())
 
 
 def embedding(text):
@@ -46,5 +36,56 @@ def embedding(text):
     embedding = tf.reduce_mean(output.last_hidden_state, axis = 1)
     return embedding.numpy().tolist()
 
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with fitz.open(pdf_path) as pdf:
+        for page in pdf:
+            text += page.get_text()
+    return text
 
-for root, dirs, files in os.walk(FOLDER)
+
+try:
+
+    # Create the Collections:
+    for collection in tqdm(collections) :
+        if not qdrant_client.collection_exists(collection_name = "document_only"):
+            print(f"\033[93m Create collection {collection} \033[0m")
+            qdrant_client.create_collection(
+                collection_name="document_only",
+                vectors_config = models.VectorParams(size=1024, distance = models.Distance.COSINE)
+            )
+            print(f"\033[92m Collection {collection} created! \033[0m")
+        else:
+            print(f"\033[92m Collection {collection} already exists! \033[0m")
+
+    print(f"Collections n Qdrant are {qdrant_client.get_collections()}")
+
+    # Embedding:
+    for idx, pdf_file in tqdm(enumerate(os.listdir(FOLDER_DOCS), start=1 )):
+        if pdf_file.endswith(".pdf"):
+            pdf_path = os.path.join(FOLDER_DOCS, pdf_file)
+
+            # Extraction = 
+            text = extract_text_from_pdf(pdf_path)
+
+            if not text.strip():
+                raise Exception (f"File {pdf_path} is empty...")
+            
+            vector = embedding(text)
+
+            point = PointStruct(
+                id = idx,
+                vector = vector,
+                payload = {
+                    "file_name": pdf_file,
+                    "content": text[:500]
+                }
+            )
+
+            qdrant_client.upsert(
+                collection_name= "document_only",
+                points= point
+            )
+    print("\033[92m Embedding Done! \033[0m")
+except Exception as e:
+    print(f"\033[91m {e} \033[0m")
