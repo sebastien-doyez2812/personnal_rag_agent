1f096ee5161c08dc9f9e25283fa3665667974e18
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-06-27 21:35:59 -0400

    modification for deployment on render

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,18 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from agent import RagAgent
 from vectordb import VectorDB
+import os
 
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173")
 
+origins = [url.strip() for url in FRONTEND_URL.split(',')]
 
-
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173" 
-]
+# For local dev:
+# origins = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:5173" 
+# ]
 
 my_vectordb = VectorDB()
 my_agent = RagAgent(vector_db= my_vectordb)
